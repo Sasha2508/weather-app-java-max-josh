5828714195cd76022eb690320d49c40094bfab89
Sasha2508 <71674667+Sasha2508@users.noreply.github.com>
2022-04-20 17:26:57 -0500

    2nd Commit. Max

--- src/main/java/com/maxjosh/weather/Main.java
+++ src/main/java/com/maxjosh/weather/Main.java
@@ -1,4 +1,4 @@
-/*
+package com.maxjosh.weather;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -6,17 +6,17 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-*/
+
     /** Set up a maven run profile in intellij or use maven from the command-line.
         Use the javafx:run argument to start the javafx application.
         Update all code and comments in this tempalte to suit your own project.
      */
-/*
+
 
     @Override
     public void start(Stage stage) {
         Label l = new Label("CS252 JavaFX Starter Template");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Scene scene = new Scene(new StackPane(l), 800, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -26,4 +26,3 @@ public class Main extends Application {
     }
 
 }
-*/
