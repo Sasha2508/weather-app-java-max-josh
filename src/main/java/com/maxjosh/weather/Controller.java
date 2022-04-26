42c1765664e877512b8d6240e8be2eb8be93171a
Sasha2508 <71674667+Sasha2508@users.noreply.github.com>
2022-04-26 12:29:46 -0500

    edited ImageProcessor

--- src/main/java/com/maxjosh/weather/Controller.java
+++ src/main/java/com/maxjosh/weather/Controller.java
@@ -1,168 +1,168 @@
-package com.maxjosh.weather;
-
-import com.maxjosh.weather.ImageProcessor;
-import com.maxjosh.weather.Forecast;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
-import javafx.animation.FadeTransition;
-import javafx.animation.PauseTransition;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class Controller implements Initializable {
-    Forecast forecastManager;
-    String citySet;
-
-    @FXML
-    private ImageView img;
-    @FXML
-    private JFXButton change, set, cancel;
-    @FXML
-    private JFXTextField cityName, invis;
-    @FXML
-    private Label city, temperature, day, desc, errors, windSpeed, cloudiness, pressure, humidity;
-
-    //Constructor to set the initial city to Ottawa
-    public Controller() {
-        this.citySet = "Ottawa".toUpperCase();
-    }
-
-    //Event Handler for each button
-    @FXML
-    private void handleButtonClicks(javafx.event.ActionEvent ae) {
-        String initialCity = city.getText(); //stores the last searched city-name
-
-        if(ae.getSource() == change){
-            cityName.setText("");
-            bottomSet(true);
-            cityName.requestFocus();
-        }else if (ae.getSource() == set) {
-            setPressed();
-        } else if (ae.getSource() == cancel) {
-            cityName.setText(initialCity);
-            bottomSet(false);
-            invis.requestFocus();
-        }
-    }
-
-    //method to clear all the fields
-    private void reset() {
-        bottomSet(false);
-        day.setText("");
-        temperature.setText("");
-        desc.setText("");
-        windSpeed.setText("");
-        cloudiness.setText("");
-        pressure.setText("");
-        humidity.setText("");
-        img.setImage(null);
-    }
-
-    //method to set the new entered city
-    private void setPressed(){
-        //if user enters nothing into cityName field
-        if(cityName.getText().equals("")){
-            showToast("City Name cannot be blank");
-            return;
-        }else {
-            try {
-                errors.setText("");
-                this.citySet = cityName.getText().trim();
-                cityName.setText((cityName.getText().trim()).toUpperCase());
-                forecastManager = new Forecast(citySet);
-                showWeather();
-                bottomSet(false);
-                invis.requestFocus();
-            }catch(Exception e){
-                city.setText("Error!!");
-                city.setTextFill(Color.TOMATO);
-                showToast("City with the given name was not found.");
-                reset();
-                invis.requestFocus();
-            }
-        }
-    }
-
-    //method to handle nodes at botton part of the scene
-    private void bottomSet(boolean statement){
-        cityName.setDisable(!statement);
-        set.setVisible(statement);
-        change.setVisible(!statement);
-        cancel.setVisible(statement);
-    }
-
-    //method to show error messages
-    private void showToast(String message) {
-        errors.setText(message);
-        errors.setTextFill(Color.TOMATO);
-        errors.setStyle("-fx-background-color: #fff; -fx-background-radius: 50px;");
-
-        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), errors);
-        fadeIn.setToValue(1);
-        fadeIn.setFromValue(0);
-        fadeIn.play();
-
-        fadeIn.setOnFinished(event -> {
-            PauseTransition pause = new PauseTransition(Duration.seconds(2));
-            pause.play();
-            pause.setOnFinished(event2 -> {
-                FadeTransition fadeOut = new FadeTransition(Duration.seconds(2), errors);
-                fadeOut.setToValue(0);
-                fadeOut.setFromValue(1);
-                fadeOut.play();
-            });
-        });
-    }
-
-    //actual method to call and get the weather and populate the scene
-    private void showWeather(){
-        forecastManager.getWeather();
-        city.setText(forecastManager.getCity().toUpperCase());
-        temperature.setText(forecastManager.getTemperature().toString()+"°C");
-        day.setText(forecastManager.getDay().toUpperCase());
-        desc.setText(forecastManager.getDescription().toUpperCase());
-        img.setImage(new Image(ImageProcessor.getImage(forecastManager.getIcon())));
-        windSpeed.setText(forecastManager.getWindSpeed()+" m/s");
-        cloudiness.setText(forecastManager.getCloudiness()+"%");
-        pressure.setText(forecastManager.getPressure()+" hpa");
-        humidity.setText(forecastManager.getHumidity()+"%");
-    }
-
-    public void initialize(URL location, ResourceBundle resources) {
-        cityName.setText(citySet);
-        cityName.setDisable(true);
-        set.setVisible(false);
-        cancel.setVisible(false);
-        errors.setText("");
-        weatherManager = new WeatherManager(citySet);
-        invis.requestFocus();
-
-        //try catch block to see if there is internet and disabling ecery field
-        try{
-            showWeather();
-        } catch (Exception e){
-            city.setText("Error!! - No Internet");
-            city.setTextFill(Color.TOMATO);
-            showToast("Internet Down. Please Connect");
-            reset();
-            change.setDisable(true);
-            cityName.setText("");
-        }
-
-        //Set the city entered into textField on pressing enter on Keyboard
-        cityName.setOnKeyPressed(e -> {
-            if(e.getCode() == KeyCode.ENTER){
-                setPressed();
-            }
-        });
-    }
-}
+//package com.maxjosh.weather;
+//
+//import com.maxjosh.weather.ImageProcessor;
+//import com.maxjosh.weather.Forecast;
+//import com.jfoenix.controls.JFXButton;
+//import com.jfoenix.controls.JFXTextField;
+//import javafx.animation.FadeTransition;
+//import javafx.animation.PauseTransition;
+//import javafx.fxml.FXML;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.Label;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import javafx.scene.input.KeyCode;
+//import javafx.scene.paint.Color;
+//import javafx.util.Duration;
+//
+//import java.net.URL;
+//import java.util.ResourceBundle;
+//
+//public class Controller implements Initializable {
+//    Forecast forecastManager;
+//    String citySet;
+//
+//    @FXML
+//    private ImageView img;
+//    @FXML
+//    private JFXButton change, set, cancel;
+//    @FXML
+//    private JFXTextField cityName, invis;
+//    @FXML
+//    private Label city, temperature, day, desc, errors, windSpeed, cloudiness, pressure, humidity;
+//
+//    //Constructor to set the initial city to Ottawa
+//    public Controller() {
+//        this.citySet = "Ottawa".toUpperCase();
+//    }
+//
+//    //Event Handler for each button
+//    @FXML
+//    private void handleButtonClicks(javafx.event.ActionEvent ae) {
+//        String initialCity = city.getText(); //stores the last searched city-name
+//
+//        if(ae.getSource() == change){
+//            cityName.setText("");
+//            bottomSet(true);
+//            cityName.requestFocus();
+//        }else if (ae.getSource() == set) {
+//            setPressed();
+//        } else if (ae.getSource() == cancel) {
+//            cityName.setText(initialCity);
+//            bottomSet(false);
+//            invis.requestFocus();
+//        }
+//    }
+//
+//    //method to clear all the fields
+//    private void reset() {
+//        bottomSet(false);
+//        day.setText("");
+//        temperature.setText("");
+//        desc.setText("");
+//        windSpeed.setText("");
+//        cloudiness.setText("");
+//        pressure.setText("");
+//        humidity.setText("");
+//        img.setImage(null);
+//    }
+//
+//    //method to set the new entered city
+//    private void setPressed(){
+//        //if user enters nothing into cityName field
+//        if(cityName.getText().equals("")){
+//            showToast("City Name cannot be blank");
+//            return;
+//        }else {
+//            try {
+//                errors.setText("");
+//                this.citySet = cityName.getText().trim();
+//                cityName.setText((cityName.getText().trim()).toUpperCase());
+//                forecastManager = new Forecast(citySet);
+//                showWeather();
+//                bottomSet(false);
+//                invis.requestFocus();
+//            }catch(Exception e){
+//                city.setText("Error!!");
+//                city.setTextFill(Color.TOMATO);
+//                showToast("City with the given name was not found.");
+//                reset();
+//                invis.requestFocus();
+//            }
+//        }
+//    }
+//
+//    //method to handle nodes at botton part of the scene
+//    private void bottomSet(boolean statement){
+//        cityName.setDisable(!statement);
+//        set.setVisible(statement);
+//        change.setVisible(!statement);
+//        cancel.setVisible(statement);
+//    }
+//
+//    //method to show error messages
+//    private void showToast(String message) {
+//        errors.setText(message);
+//        errors.setTextFill(Color.TOMATO);
+//        errors.setStyle("-fx-background-color: #fff; -fx-background-radius: 50px;");
+//
+//        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), errors);
+//        fadeIn.setToValue(1);
+//        fadeIn.setFromValue(0);
+//        fadeIn.play();
+//
+//        fadeIn.setOnFinished(event -> {
+//            PauseTransition pause = new PauseTransition(Duration.seconds(2));
+//            pause.play();
+//            pause.setOnFinished(event2 -> {
+//                FadeTransition fadeOut = new FadeTransition(Duration.seconds(2), errors);
+//                fadeOut.setToValue(0);
+//                fadeOut.setFromValue(1);
+//                fadeOut.play();
+//            });
+//        });
+//    }
+//
+//    //actual method to call and get the weather and populate the scene
+//    private void showWeather(){
+//        forecastManager.getWeather();
+//        city.setText(forecastManager.getCity().toUpperCase());
+//        temperature.setText(forecastManager.getTemperature().toString()+"°C");
+//        day.setText(forecastManager.getDay().toUpperCase());
+//        desc.setText(forecastManager.getDescription().toUpperCase());
+//        img.setImage(new Image(ImageProcessor.getImage(forecastManager.getIcon())));
+//        windSpeed.setText(forecastManager.getWindSpeed()+" m/s");
+//        cloudiness.setText(forecastManager.getCloudiness()+"%");
+//        pressure.setText(forecastManager.getPressure()+" hpa");
+//        humidity.setText(forecastManager.getHumidity()+"%");
+//    }
+//
+//    public void initialize(URL location, ResourceBundle resources) {
+//        cityName.setText(citySet);
+//        cityName.setDisable(true);
+//        set.setVisible(false);
+//        cancel.setVisible(false);
+//        errors.setText("");
+//        weatherManager = new WeatherManager(citySet);
+//        invis.requestFocus();
+//
+//        //try catch block to see if there is internet and disabling ecery field
+//        try{
+//            showWeather();
+//        } catch (Exception e){
+//            city.setText("Error!! - No Internet");
+//            city.setTextFill(Color.TOMATO);
+//            showToast("Internet Down. Please Connect");
+//            reset();
+//            change.setDisable(true);
+//            cityName.setText("");
+//        }
+//
+//        //Set the city entered into textField on pressing enter on Keyboard
+//        cityName.setOnKeyPressed(e -> {
+//            if(e.getCode() == KeyCode.ENTER){
+//                setPressed();
+//            }
+//        });
+//    }
+//}
--- src/main/java/com/maxjosh/weather/Main.java
+++ src/main/java/com/maxjosh/weather/Main.java
@@ -2,8 +2,11 @@ package com.maxjosh.weather;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 
 import com.maxjosh.weather.Forecast;
 
@@ -17,15 +20,31 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label l = new Label("CS252 JavaFX Starter Template");
-        Scene scene = new Scene(new StackPane(l), 800, 480);
+        Label city = new Label("City:");
+        TextField cityName = new TextField();
+        Label info = new Label();
+        Label deg = new Label();
+        Button submit = new Button("Submit");
+
+        submit.setOnAction(e -> {
+           info.setText("Loading forecast for: " + cityName.getText());
+        });
+
+        var rootPane = new FlowPane();
+        rootPane.getChildren().add(l);
+        rootPane.getChildren().add(cityName);
+        rootPane.getChildren().add(submit);
+        rootPane.getChildren().add(info);
+        rootPane.getChildren().add(deg);
+
+        Scene scene = new Scene(rootPane, 800, 480);
         stage.setScene(scene);
         stage.show();
-        Forecast forecast = new Forecast("Kansas City");
-
+        Forecast forecast = new Forecast("Ottawa");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 
