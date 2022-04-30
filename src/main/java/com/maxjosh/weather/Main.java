1a71fb2bc57057dc9a4c69f65792952fbbf85692
Sasha2508 <71674667+Sasha2508@users.noreply.github.com>
2022-04-30 11:31:35 -0500

    configuring more boxes, and styling

--- src/main/java/com/maxjosh/weather/Main.java
+++ src/main/java/com/maxjosh/weather/Main.java
@@ -33,21 +33,36 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        //let's use Border pane to set layout
+        /*******************************************
+         *
+         * Creating @border instance of BorderPane type
+         * putting median boxes inside
+         * @rightVbox
+         * @leftVbox
+         * @centerVbox
+         * @bottomVbox
+         * @centerVbox
+         *
+         *
+        * ********************************************/
+
         BorderPane border = new BorderPane();
         var rightVbox = addVBox();
+        var leftVbox = addVBox();
+        var centerVbox = addVBox();
+        var bottomVbox = addVBox();
         border.setRight(rightVbox);
+        border.setLeft(leftVbox);
+        border.setCenter(centerVbox);
+        border.setBottom(bottomVbox);
+        /*****************************************
+        * Labels that are inside inner containers
+         * @city
+         * @temperature, @humidity, @wind, @press,
+         * @image, @cloud, @day, @desc,
+         * @submit button
+        *******************************************/
 
-//        HBox hbox = addHBox();
-//        border.setTop(hbox);
-//        border.setLeft(addVBox);
-//        addStackPane(hbox); //Adding stack to Hbox in top region
-//        border.setCenter(addFlowPane());
-//        border.setRight(addFlowPane());
-
-        //labels
-
-//        Label l = new Label("City:");
         TextField cityName = new TextField();
         Label city = new Label(); // example
         Label temperature = new Label();
@@ -59,10 +74,17 @@ public class Main extends Application {
         Label day = new Label();
         Label desc = new Label();
 
+        /******************
+        *creating button called
+         * @submit
+        **** ***************/
         Button submit = new Button("Submit");
 
 
-
+        /****************
+        * eventHandler
+        *
+        ****************/
         submit.setOnAction(e -> {
             //deg.setText("Loading forecast for: " + cityName.getText());
             var forecast = new Forecast(cityName.getText().substring(0,1).toUpperCase(Locale.ROOT)+cityName.getText().substring(1, cityName.getLength()));
@@ -91,60 +113,68 @@ public class Main extends Application {
 
         //nextForecast.setOnAction()e ->){};
 //        var stackPane = new StackPane();
-
+/***********************************
+*Using flowPane as inner containers
+ * @topPane
+ * @bottomPane
+ * @leftPane
+ * @rightPane
+ * @centerPane
+***********************************/
         var topPane = new FlowPane();
         var bottomPane = new FlowPane();
         var leftPane = new FlowPane();
-        var rightPane = new FlowPane();
+        var rightPaneValues = new FlowPane();
+        var rightPaneKeys = new FlowPane();
         var centerPane = new FlowPane();
-//        var picturePane = new FlowPane();
 
 
-        //centerPane with all the data
-//        topPane.getChildren().add(l);
+
+        /*****************************
+         * adding labels to containers
+         *
+        **********************************/
         bottomPane.getChildren().add(cityName);
         bottomPane.getChildren().add(submit);
         topPane.getChildren().add(city);
-        rightPane.getChildren().add(temperature);
-        rightPane.getChildren().add(humidity);
-        rightPane.getChildren().add(wind);
-        rightPane.getChildren().add(press);
-        rightPane.getChildren().add(cloud);
+//        topPane.setAlignment();
+        rightPaneValues.getChildren().add(temperature);
+        rightPaneValues.getChildren().add(humidity);
+        rightPaneValues.getChildren().add(wind);
+        rightPaneValues.getChildren().add(press);
+        rightPaneValues.getChildren().add(cloud);
         leftPane.getChildren().add(day);
         centerPane.getChildren().add(desc);
 
-        //picturePane
-//        picturePane.getChildren().add(city);
-
-        rightVbox.getChildren().add(rightPane);
-
-
-        //set orientation and Hgap and Vgap
-        rightPane.setOrientation(Orientation.VERTICAL);
-        rightPane.setHgap(10);
-        rightPane.setVgap(10);
+        /*****************************************
+         * adding innerContainer to median-containers
+        ********************************************/
+        rightVbox.getChildren().add(rightPaneValues);
 
 
-        //stackpane
-//        stackPane.getChildren().addAll( picturePane, centerPane);
+        /*******************************************
+         * setting orientation
+         * inside inner containers
+         * then set it to outer containers
+         *******************************************/
+        rightPaneValues.setOrientation(Orientation.VERTICAL);
+        rightPaneValues.setHgap(10);
+        rightPaneValues.setPadding(new Insets(40, 10, 10, 10));
+        rightPaneValues.setVgap(10);
+        rightPaneValues.setStyle("-fx-background-color: #336699;");
+        rightPaneValues.
+
+
         border.setCenter(centerPane);
         border.setTop(topPane);
         border.setBottom(bottomPane);
         border.setLeft(leftPane);
 
-
-
-
-
-
-
         //defining scene
-        Scene scene = new Scene(border, 600, 500);
+        Scene scene = new Scene(border, 900, 500);
         stage.setScene(scene);
         stage.setTitle("Weather App");
         stage.show();
-
-
     }
 
 //    private HBox addHBox() {
@@ -157,9 +187,10 @@ public class Main extends Application {
 
     private VBox addVBox(){
         VBox vbox = new VBox();
-        vbox.setPadding(new Insets(10));
+        vbox.setPadding(new Insets(100));
         vbox.setSpacing(8);
-        Text title = new Text("Title");
+//        vbox.setBorder(new Border());
+        Text title = new Text("Weather Details");
         title.setFont(new Font("Arial", 24));
         vbox.getChildren().add(title);
 
